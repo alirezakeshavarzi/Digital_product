39192ba3b8c60bcc325e75dc028b36ced931edb2
Alireza.k <ahmidyly@gmail.com>
2023-07-24 16:01:12 +0330

    add categories field to Product class

--- digital_products/products/models.py
+++ digital_products/products/models.py
@@ -15,17 +15,21 @@ class Category(models.Model):
         verbose_name='Category' # Show this name in admin panel.
         verbose_name_plural = 'Categories' # برای اسم جمع
 
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(blank=True, upload_to='products/')
     is_enable = models.BooleanField(_('is enable'), default=True, )
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     create_time = models.DateTimeField(_('create time'), auto_now_add=True)
     update_time = models.DateTimeField(_('update time'), auto_now=True)
 
     class Meta:
         db_table = 'products' # A table that create in databases.
-        verbose_name= _('product') # Show this name in admin panel.
+        verbose_name= _('adas') # Show this name in admin panel.
         verbose_name_plural = _('products') # برای اسم جمع
 
 
@@ -35,7 +39,7 @@ class File(models.Model):
     title = models.CharField(_('title'), max_length=50)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
     is_enable = models.BooleanField(_('is enable'), default=True, )
-    categoties = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     create_time = models.DateTimeField(_('create time'), auto_now_add=True)
     update_time = models.DateTimeField(_('update time'), auto_now=True)
 
