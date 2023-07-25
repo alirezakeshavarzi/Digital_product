a910d747c133526ffda51782fd8cb833d668ac58
Alireza.k <ahmidyly@gmail.com>
2023-07-25 15:59:00 +0330

    add path with int id to show detail each product

--- digital_products/products/urls.py
+++ digital_products/products/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductListView, ProductDetailsView
 urlpatterns = [
     path('products/', ProductListView.as_view()),
-    #path('categores/', CategoryListView.as_view()),
+    path('products/<int:id>/', ProductDetailsView.as_view()),
 ]
