1918cf3093fb1065457634275d0e5e5122c232ba
Alireza.k <ahmidyly@gmail.com>
2023-07-25 15:29:51 +0330

    add 'many=True' to ProductSerializer

--- digital_products/products/views.py
+++ digital_products/products/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product, File, Category
+
+from .serializers import ProductSerializer
+
+class ProductListView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+'''class CategoryListView(APIView):
+
+    def get(self, request):
+        categorys = Category.objects.all()
+        serializer = Serializer(categorys,many=True)
+        return Response(serializer.data)
+'''
+
+
+
 # Create your views here.
